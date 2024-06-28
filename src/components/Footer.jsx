6030e0b3738c9cb8d6dff2f8018992e683a0a9d6
EmmyRecent <chukwuemekaeziagulu@gmail.com>
2024-06-28 11:44:07 +0100

    Add Dynamic footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
 const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+
   return (
     <footer className="w-full py-4 text-center">
-      <p className="text-[#ccc]">Copyright C 2024</p>
+      <p className="text-[#ccc]">
+        Made with 💌 by{" "}
+        <a href="https://chukwuemeka-e.vercel.app/" className="cursor-pointer">
+          Chukwuemeka
+        </a>
+      </p>
+      <p className="text-base text-[#ccc]">Copyright &copy; {year}</p>
     </footer>
   );
 };
